3a7cefed8fe10c2d7f39f05901fb6561c14fba5d
hanford <jackhanford@gmail.com>
2018-03-05 11:29:48 -0800

    make some changes? 👀

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import styled from 'styled-components/native'
-import kmToM from 'km-m'
 import sortOn from 'sort-on'
 
-import { yelp } from './config'
+import { yelp, token } from './config'
 import fixture from './fixture'
 
 import {
@@ -52,18 +51,34 @@ export default class HomeScreen extends React.Component {
   }
 
   visit = async place => {
-    console.log(place)
+    var serializeJSON = function(data) {
+      return Object.keys(data).map(function (keyName) {
+        return encodeURIComponent(keyName) + '=' + encodeURIComponent(data[keyName])
+      }).join('&');
+    }
+
+    let body = {
+      method: 'POST',
+      headers: {
+        'x-auth-token': token
+      },
+      body: serializeJSON({ business_id: place.id, location_service_types: 'wifi' })
+    }
+
+    const url = `https://auto-api.yelp.com/check_in?accuracy=0.007456&app_version=12.1.0&cc=US&device_type=iPhone10%2C6%2F11.2.1&efs=0HweRIEwPM0OnQpUuqU6ki9efWSQIVNIck8W9lIWp5VIVKpdpKPQldQWZzXUx9sw4L4lcssBTqs9wnOXLxHORWXJRhzMgCSzydKL5hXU%2BTryIg200sPGECvVPdnyj%2FrK&lang=en&locale=en_US&nonce=F57B85CE-835F-4D6A-942D-969E892048F5&signature=OYYpGesvlbhqjuwEoagCYQ%3D%3D&time=${Date.now()}&ywsid=gpfz_nbS33avFWL0Ozgz3Q`
+
+    console.log(body)
+    try {
+      const res = await fetch(url, body)
+      console.log(res)
+    } catch (err) {
+      console.error(err)
+    }
+
   }
 
   getBusinesses = async () => {
-    let body = {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${yelp}`
-      }
-    }
-
-    const res = await fetch(`https://api.yelp.com/v3/businesses/search?latitude=${this.state.latitude}&longitude=${this.state.longitude}`, body)
+    const res = await fetch(`https://auto-api.yelp.com/nearby/suggest?accuracy=0.040389&app_version=12.1.0&cc=US&device_type=iPhone10%2C6%2F11.2.1&efs=hJ4yK9kdYRPeEHYOBjz23NJpEEJqZ9nTx2%2FlMhrIoW8lloANKI3HItn7FsM8FQR1rEGf9pTrzwYO%2Fye0slYSxy9%2BJPQcEJwjKyXi1%2BbtiGxfKOcQQxhnq5e92JZFA82V&lang=en&locale=en_US&signature=lJiT4NfX%2BzsC8F4YAsBArQ%3D%3D&time=1514854306&ywsid=gpfz_nbS33avFWL0Ozgz3Q`)
     let { businesses } = await res.json()
 
     businesses = sortOn(businesses, 'distance')
@@ -91,7 +106,7 @@ export default class HomeScreen extends React.Component {
               <ScrollContainer>
                 {
                   this.state.businesses.map((place, index) => {
-                    const uri = place.image_url === '' ? this.state.businesses[index - 1].image_url : place.image_url
+                    const uri = place.image_url === '' ? this.state.businesses[index - 1].photo_url : place.photo_url
 
                     return (
                       <Item
@@ -100,11 +115,11 @@ export default class HomeScreen extends React.Component {
                       >
                         <View style={{ flex: 1 }}>
                           <Preview source={{ uri }} />
-                          <BlurView tint='dark' intensity={95} style={StyleSheet.absoluteFill}>
+                          <BlurView tint='dark' intensity={90} style={StyleSheet.absoluteFill}>
                             <Info>
                               <Title>{place.name}</Title>
                               <Subtitle>{place.review_count} reviews</Subtitle>
-                              <Subtitle>{kmToM(place.distance / 1000).toFixed(0)} miles away</Subtitle>
+                              {/* <Subtitle>{kmToM(place.distance / 1000).toFixed(0)} miles away</Subtitle> */}
                             </Info>
                           </BlurView>
                         </View>
